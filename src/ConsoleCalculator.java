425d66efe0bbe9bbcae0cea2dd7856356044aa45
syike20 <dashrathrao28@gmail.com>
2026-09-22 22:36:48 +0530

    Add Console Calculator

--- src/ConsoleCalculator.java
+++ src/ConsoleCalculator.java
@@ -1,86 +1,72 @@
+
+//# Console Calculator
+
+/*A simple Java console-based calculator designed to practice 
+and demonstrate fundamental programming concepts, including variables and data types, 
+operators, conditional statements, switch-case, loops, methods, and user input handling.*/
+
 import java.util.Scanner;
-public class ConsoleCalculator {
+class ConsoleCalculator{
+    public static double Addition(double n1, double n2){
+        return n1 + n2 ;
+    }
+    public static double Subtraction(double n1, double n2){
+        return n1 - n2 ;
+    }
+    public static double Multiplication(double n1 , double n2){
+        return n1 * n2 ;
+    }
+    public static double Division(double n1 , double n2){
+        if(n2==0){
+            System.out.println("Mathematical Error !! Division by Zero.");
+            return 0;
+        }
+        return n1 / n2 ;
+    }
+
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        //TITLE
         System.out.println("------------------");
         System.out.println("CONSOLE CALCULATOR");
         System.out.println("------------------");
-        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+
         
-       while(true){ 
+        do{
+        //USER INPUT 
+            System.out.print("Enter First Operand  : ");
+            double num1 = scanner.nextDouble();
+            System.out.print("Enter Second Operand : ");
+            double num2 = scanner.nextDouble();
 
+            System.out.print("Enter Operation Type (+,-,/,*) : ");
+            String operation = scanner.next();
 
-            System.out.println("Choose operation to perform");
-
-            System.out.println("Enter 1 for Addition");
-            System.out.println("Enter 2 for Subtraction");
-            System.out.println("Enter 3 for Multiplication");
-            System.out.println("Enter 4 for Division");
-            System.out.println("Enter 5 for EXIT");
-
-            int choice = scanner.nextInt();
-
-            switch(choice){
-                case 1 -> {
-                    System.out.print("Enter your first operand:");
-                    int a = scanner.nextInt();
-                    System.out.print("Enter your second operand:");
-                    int b = scanner.nextInt();
-                    int result = a + b ; 
-                    System.out.println("Result : " + result );
-
-                }
-                case 2 -> {
-                    System.out.print("Enter your first operand:");
-                    int a = scanner.nextInt();
-                    System.out.print("Enter your second operand:");
-                    int b = scanner.nextInt();
-                    int result  = a - b ;
-                    System.out.println("Result : " + result );
-
-                }
-                case 3 -> {
-                    System.out.print("Enter your first operand:");
-                    int a = scanner.nextInt();
-                    System.out.print("Enter your second operand:");
-                    int b = scanner.nextInt();
-                    int result = a * b ;
-                    System.out.println("Result : " + result );
-
-                }
-                case 4 -> {
-                    System.out.print("Enter your first operand:");
-                    double a = scanner.nextInt();
-                    System.out.print("Enter your second operand:");
-                    double b = scanner.nextInt();
-                    if(b==0){
-                        System.out.println("Logic Error : Division by Zero.");
-                        break;
-                    }
-                    double result = a / b ;
-                    System.out.println("Result : " + result );
-
-                }
-                case 5 -> {
-                    System.out.println("CLOSING......please wait.");
-                    System.out.println("CALCULATOR CLOSED");
-                    scanner.close();
-                    return;
-                }
-                default -> {
-                    System.out.print("Invalid Choice!Try Again.");
-                }
+            switch(operation){
+                case "+" -> System.out.println("Result :" + Addition(num1,num2));
+                case "-" -> System.out.println("Result :" +Subtraction(num1,num2));
+                case "*" -> System.out.println("Result :" +Multiplication(num1,num2));
+                case "/" -> System.out.println("Result :" +Division(num1,num2));
+                default ->  System.out.println(" Enter A Valid Coice !! "); 
             }
-
             
-        }
+            System.out.print("To Continue Type 'CONTINUE' OR To Exit Type 'EXIT' : " );
+            String choice = scanner.next();
+            if(choice.equalsIgnoreCase("EXIT")){
+                break;
+            }else if(choice.equalsIgnoreCase("CONTINUE")){
+                continue;
+            }
+            else{
+                break;
+            }
+        }while(true);
+        scanner.close();
+        return;
         
+
+
+
     }
-
-
-
 }
-   
-
-
-
-    
